3ae723f0612aeedb05a146b83880cda1deb30c85
Sebastian Lövdahl <slovdahl@hibox.fi>
2023-06-26 22:01:24 +0300

    Fix diagnostics after spothinta-api version bump

--- custom_components/spothinta/diagnostics.py
+++ custom_components/spothinta/diagnostics.py
@@ -26,12 +26,12 @@ async def async_get_config_entry_diagnostics(
         "energy": {
             "current_hour_price": coordinator.data.energy_today.current_price,
             "next_hour_price": coordinator.data.energy_today.price_at_time(
-                coordinator.data.energy_today.utcnow() + timedelta(hours=1)
+                coordinator.data.energy_today.now_in_timezone() + timedelta(hours=1)
             ),
-            "average_price": coordinator.data.energy_today.average_price,
-            "max_price": coordinator.data.energy_today.extreme_prices[1],
-            "min_price": coordinator.data.energy_today.extreme_prices[0],
-            "highest_price_time": coordinator.data.energy_today.highest_price_time,
-            "lowest_price_time": coordinator.data.energy_today.lowest_price_time,
+            "average_price": coordinator.data.energy_today.average_price_today,
+            "max_price": coordinator.data.energy_today.highest_price_today,
+            "min_price": coordinator.data.energy_today.lowest_price_today,
+            "highest_price_time": coordinator.data.energy_today.highest_price_time_today,
+            "lowest_price_time": coordinator.data.energy_today.lowest_price_time_today,
         },
     }
