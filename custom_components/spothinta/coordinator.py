40491dd7c8f58f0afe7a1e3a095621c88582f0a0
Sebastian Lövdahl <slovdahl@hibox.fi>
2026-03-31 20:55:45 +0300

    Make sure we poll for prices again if the set was incomplete
    It seems like the Spot-Hinta.fi API has started returning incomplete data for
    some time after day-ahead prices are published. With the current logic we stop
    polling for prices if we get at least one price for tomorrow. From now on we
    also try to poll again if we notice that some future prices are missing.

--- custom_components/spothinta/coordinator.py
+++ custom_components/spothinta/coordinator.py
@@ -1,7 +1,7 @@
 """The Coordinator for Spot-Hinta.fi."""
 from __future__ import annotations
 
-from datetime import timedelta
+from datetime import datetime, timedelta
 import logging
 from random import randint
 from typing import NamedTuple
@@ -56,21 +56,21 @@ class SpotHintaDataUpdateCoordinator(DataUpdateCoordinator[SpotHintaData]):
 
         now = dt_util.utcnow()
 
-        if self.current_data is not None and has_prices_for_tomorrow(self.current_data):
+        # The normal happy path, we have the enough for prices for today and
+        # tomorrow, no need to trigger any external requests.
+        if self.current_data is not None and has_prices_for_tomorrow_until_next_day_refresh(self.current_data):
             # Trigger an update of the sensors at the next quarter of the hour.
-            next_update_at = now + timedelta(minutes=15)
-            next_minute = next_update_at.minute // 15 * 15
-            next_update_at = next_update_at.replace(
-                minute=next_minute, second=0, microsecond=0, tzinfo=dt_util.UTC
-            )
-
+            next_update_at = get_next_quarter_of_hour(now)
             self.future_update = async_track_point_in_time(
                 self.hass, self.async_request_update, next_update_at
             )
 
             return SpotHintaData(energy_today=self.current_data)
 
-        if self.current_data is None or now.hour >= 11:
+        # Day-ahead prices are usually published around 13-14:00 CET. Depending
+        # on the time of the year, this is either 11-12:00 or 12-13:00 UTC. We
+        # start polling for new prices at 11:00 UTC.
+        if self.current_data is None or now.hour >= 11 or not has_prices_for_tomorrow_until_next_day_refresh(self.current_data):
             random_delay = randint(0, 120)
 
             # We want to get the prices for tomorrow, but we want to avoid
@@ -90,6 +90,8 @@ class SpotHintaDataUpdateCoordinator(DataUpdateCoordinator[SpotHintaData]):
             except SpotHintaConnectionError as err:
                 _LOGGER.warning("Failed to get energy prices", exc_info=True)
 
+                # The refresh failed, but we already have some data which we can
+                # return while scheduling a new refresh in a few minutes.
                 if self.current_data is not None:
                     next_update_at = (
                         now + timedelta(minutes=5) + timedelta(seconds=random_delay)
@@ -106,17 +108,22 @@ class SpotHintaDataUpdateCoordinator(DataUpdateCoordinator[SpotHintaData]):
                     "Error communicating with Spot-Hinta.fi API"
                 ) from err
 
-        if has_prices_for_tomorrow(self.current_data):
-            _LOGGER.debug("Got prices for tomorrow")
-            next_update_at = now.replace(minute=0, second=0) + timedelta(hours=1)
-        elif now.hour >= 11:
+        if has_prices_for_tomorrow_until_next_day_refresh(self.current_data):
+            _LOGGER.debug("Got prices until tomorrow's refresh")
+            next_update_at = get_next_quarter_of_hour(now)
+        elif now.hour >= 11 and not has_prices_for_tomorrow(self.current_data):
+            # Try again in a few minutes if there were no prices at all for
+            # tomorrow. This can happen if the prices for tomorrow haven't
+            # been published yet.
             next_update_at = now + timedelta(minutes=3)
             _LOGGER.debug(
                 "Tomorrow's prices not yet available, next check: %s",
                 str(next_update_at),
             )
         else:
-            next_update_at = now.replace(minute=0, second=0) + timedelta(hours=1)
+            # We got some prices but not yet until the next day refresh.
+            _LOGGER.info("Not all prices for tomorrow available from the API yet")
+            next_update_at = get_next_quarter_of_hour(now)
 
         _LOGGER.debug("Next update: %s", str(next_update_at))
         self.future_update = async_track_point_in_time(
@@ -126,6 +133,34 @@ class SpotHintaDataUpdateCoordinator(DataUpdateCoordinator[SpotHintaData]):
         return SpotHintaData(
             energy_today=self.current_data,
         )
+
+
+def get_next_quarter_of_hour(now: datetime) -> datetime:
+    """Get the next quarter of the hour."""
+    next_update_at = now + timedelta(minutes=15)
+    next_minute = next_update_at.minute // 15 * 15
+    next_update_at = next_update_at.replace(
+        minute=next_minute, second=0, microsecond=0, tzinfo=dt_util.UTC
+    )
+    return next_update_at
+
+
+def has_prices_for_tomorrow_until_next_day_refresh(energy_prices: Electricity | None) -> bool:
+    """Returns true if there are prices for tomorrow until the next day refresh in the given energy prices"""
+    if energy_prices is None:
+        return False
+
+    now = dt_util.utcnow()
+    for timestamp in energy_prices.prices.keys():
+        # The prices for tomorrow are usually published around 13-14:00 CET.
+        # Depending on the time of the year, this is either 11-12:00 or
+        # 12-13:00 UTC. If we have prices until 12:00 UTC tomorrow, we can
+        # wait with polling for new prices until the next day refresh, even
+        # if we don't have prices for the whole tomorrow yet.
+        if timestamp.day > now.day and timestamp.hour >= 12:
+            return True
+
+    return False
 
 
 def has_prices_for_tomorrow(energy_prices: Electricity | None) -> bool:
